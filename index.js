cee569a1ca6008ff27c3e0a571119c4dde48db5c
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-19 22:14:35 +0500

    Node-JS-Events

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const http = require("http");
 
-const server = http.createServer((request, response) => {
-  // request - sorov
-  // respon - javob
-  console.log(request.url);
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
 
-  response.write("<h1>Hello world 1</h1>");
-  response.write("<h1>Hello world 2</h1>");
-  response.end();
+    res.end(`
+            <h2>Send name</h2>
+            <form method="post" action="/">
+                <input name="name" placeholder="Enter your name" />
+                <button type="submit">Send name</button>
+            </form>
+        `);
+  } else if (req.method === "POST") {
+    const body = [];
+
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+
+      res.end(`name successfully added: ${message}`);
+    });
+  }
 });
 
 server.listen(3000, () => {
-  console.log("Server has beeen started on port 3000");
+  console.log("Server has been started on port: 3000");
 });
